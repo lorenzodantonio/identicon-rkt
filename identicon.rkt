7928474c62a7ad3aa38c84c5034186b9baf1b2e5
Lorenzo <lorenzo.dantonio@gmail.com>
2025-09-09 02:34:26 +0200

    style: format

--- identicon.rkt
+++ identicon.rkt
@@ -11,27 +11,25 @@
 (define (with-index enumerable index)
   (if (null? enumerable)
       '()
-      (cons (list (car enumerable) index)
-            (with-index (cdr enumerable) (+ 1 index)))))
+      (cons (list (car enumerable) index) (with-index (cdr enumerable) (+ 1 index)))))
 
 (define (chunk enum step)
   (if (< (length enum) step)
       '()
-      (cons (take enum step)
-            (chunk (drop enum step) step))))
+      (cons (take enum step) (chunk (drop enum step) step))))
 
 (define (hash-input string)
   (bytes->list (md5 string)))
 
 (define (pick-color hash-list)
-  (define (normalize c) (+ 80 (modulo c 176)))
+  (define (normalize c)
+    (+ 80 (modulo c 176)))
   (list->vector (map normalize (take hash-list 3))))
 
 (define (mirror-h row)
-  (let* (
-    [one (first row)]
-    [two (second row)]
-    [three (third row)])
+  (let* ([one (first row)]
+         [two (second row)]
+         [three (third row)])
     (list one two three two one)))
 
 (define (build-grid hash-list)
@@ -42,20 +40,22 @@
     (filter (lambda (x) (even? (first x))) indexed)))
 
 (define (build-svg grid color)
-    (svg-content image-len image-len
-      (apply string-append
-        (map (lambda (p)
-              (svg-rect (* unit-len (modulo (second p) 5))
-                        (* unit-len (quotient (second p) 5))
-                        unit-len unit-len
-                        color))
-            grid))))
+  (svg-content image-len
+               image-len
+               (apply string-append
+                      (map (lambda (p)
+                             (svg-rect (* unit-len (modulo (second p) 5))
+                                       (* unit-len (quotient (second p) 5))
+                                       unit-len
+                                       unit-len
+                                       color))
+                           grid))))
 
 (define (save-to-file svg-content file-name output-dir)
   (let ([img-path (build-path output-dir (string-append file-name ".svg"))])
     (call-with-output-file* img-path
-      (lambda (out) (display svg-content out))
-      #:exists 'truncate/replace)))
+                            (lambda (out) (display svg-content out))
+                            #:exists 'truncate/replace)))
 
 (define (identicon string output-dir)
   (let* ([img (hash-input string)]
--- main.rkt
+++ main.rkt
@@ -4,11 +4,9 @@
 (define output-dir "output")
 
 (define (main)
-  (command-line
-    #:args (input-string)
-    (unless (directory-exists? output-dir)
-      (make-directory output-dir))
-
-    (identicon input-string output-dir)))
+  (command-line #:args (input-string)
+                (unless (directory-exists? output-dir)
+                  (make-directory output-dir))
+                (identicon input-string output-dir)))
 
 (main)
